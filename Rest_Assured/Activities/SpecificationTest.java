105b0ff85d49ab57e0ab5aceb573e8027913074b
KokilaJagtap <kokilajagtap@in.ibm.com>
2024-09-09 17:39:59 +0530

    Add files via upload

--- Rest_Assured/Activities/SpecificationTest.java
+++ Rest_Assured/Activities/SpecificationTest.java
@@ -3,16 +3,19 @@ package examples;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.response.Response;
-import io.restassured.specification.*;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
+
+import static io.restassured.RestAssured.given;
+
 
 public class SpecificationTest {
     RequestSpecification requestSpec;
@@ -21,39 +24,45 @@ public class SpecificationTest {
 
     @BeforeClass
     public void setUp() {
+
+        //Request Specification
         requestSpec = new RequestSpecBuilder()
                 .setBaseUri("https://petstore.swagger.io/v2/pet")
                 .addHeader("Content-Type", "application/json")
                 .build();
+
+        //Response Specification
         responseSpec = new ResponseSpecBuilder()
                 .expectStatusCode(200)
-                .expectBody("status", equalTo("alive"))
-                .expectResponseTime(lessThanOrEqualTo(3000L))
+                .expectBody("status",equalTo("alive"))
+                .expectResponseTime(lessThanOrEqualTo(3000l))
                 .build();
     }
-
     @Test(priority = 1)
     public void postRequest(){
         Map<String, Object> reqBody = new HashMap<>();
-        reqBody.put("id", "2039");
-        reqBody.put("name", "Shifu");
-        reqBody.put("status", "alive");
-        Response response = given().spec(requestSpec).body(reqBody).when().post();
+        reqBody.put("id",777232);
+        reqBody.put("name","Rilley");
+        reqBody.put("status","alive");
 
-        petId = response.then().extract().path("id");
+        Response response =  given().spec(requestSpec).body(reqBody).when().post();
+        petId= response.then().extract().path("id");
+
         response.then().spec(responseSpec);
     }
 
     @Test(priority = 2)
-    public void getRequest(){
-        given().spec(requestSpec).pathParam("petId", petId).
-                when().get("/{petId}").then().spec(responseSpec);
+    public void getResponse(){
+        given().spec(requestSpec).pathParams("petId",petId).
+                when().get("/{petId}").
+                then().spec(responseSpec);
     }
 
     @Test(priority = 3)
     public void deleteRequest(){
-        given().spec(requestSpec).pathParam("petId", petId).
-                when().delete("/{petId}").then().statusCode(200).
-                body("message", equalTo("" + petId));
+        given().spec(requestSpec).pathParams("petId",petId).
+                when().delete("/{petId}").
+                then().statusCode(200).body("message",equalTo(""+petId));
     }
+
 }
